de80c1960ad4cd0845acf25c9c7477bb7f400949
Robert Breunung <rbreunung@gmail.com>
2023-11-29 15:48:03 +0100

    first structure

---
+++ export.ps1
@@ -0,0 +1,33 @@
+# I implement this script with latest Powershell available an do not intend to test older ones. It may or may be not working for Powershell 5.
+#Requires -Version 7.0
+
+# https://learn.microsoft.com/de-de/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4
+# ensure the output is UTF8
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+function New-ExportFolder {
+    $exportFolderName = "export-$(Get-Date -Format "yyyy-MM-dd-HH-mm")"
+    $ExportDirectory = Join-Path -Path (Get-Location) -ChildPath $exportFolderName
+    New-Item -Path $ExportDirectory -ItemType Directory
+    return $ExportDirectory
+}
+
+# check bitwarden unlocked
+$bitwardenData = Invoke-Expression "bw status"  | ConvertFrom-Json -Depth 10
+if ( -not (($bitwardenData.status) -eq "unlocked") ) {
+    Write-Error "Bitwarden is not unlocked! Please unlock your Bitwarden CLI."
+    Pause
+    return -1
+}
+
+# create the export directory
+$ExportDirectory = New-ExportFolder
+
+# Read data from Bitwarden
+$bitwardenData = Invoke-Expression "bw list items"  | ConvertFrom-Json -Depth 10
+foreach ($bitwardenElement in $bitwardenData) {
+    Write-Output $bitwardenData
+    break
+}
+
+
