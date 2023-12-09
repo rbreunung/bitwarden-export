9437a990d71dc6c79a1bdca14b938476a0127dbf
Robert Breunung <rbreunung@gmail.com>
2023-12-09 20:29:40 +0100

    import script state

--- import.ps1
+++ import.ps1
@@ -1,18 +1,30 @@
 #Requires -Version 7.0
 
 param (
-    [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Who do you want to greet?")]
+    [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Provide the target path of the export script.")]
     [string]$Path
 )
 
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
 if (-not (Test-Path $Path -PathType Container)) {
-    Write-Error "The path $Path is not existing. Please provide a valid export path from the export script."
+    Write-Error "The path `"$Path`" is not existing. Please provide a valid export path from the export script."
     exit -1
 }
 
 foreach ($element in Get-ChildItem $Path -Directory) {
-    Write-Output "Processing element $element"
+    $name = $element.Name
+    $itemPath = Join-Path $element "$name.json"
+    if (-not (Test-Path $itemPath -PathType Leaf)) {
+        Write-Warning "The folder $name does not contain a Bitwarden item file $name.json. Skipping!"
+        continue
+    }
+    Write-Output "Processing element $name"
+    $bitwardenElement = Get-Content $itemPath | ConvertFrom-Json -Depth 10
+    foreach ($attachment in $bitwardenElement.attachments) {
+        Write-Output "Importing `"$($attachment.fileName)`" for entry $($bitwardenElement.name)"
+    }
+
+
 }
 
